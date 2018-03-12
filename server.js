b11f50b4ebf9f4ad9235338802debe362f1d8c33
daprieto1 <da.prieto1@uniandes.edu.co>
2018-03-12 17:02:32 -0500

    heroku cli

--- server.js
+++ server.js
@@ -49,4 +49,4 @@ var port = process.env.PORT || 8080;
 app.listen(port);
 console.log("App listening on port 8080");
 var UtilsService = require('./UtilsService');
-UtilsService.executeCommand('pwd');
+UtilsService.executeCommand('heroku run pwd');
