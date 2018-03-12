1d92502b0fe8c4ab370544e2928db64877c1cef3
daprieto1 <da.prieto1@uniandes.edu.co>
2018-03-12 15:09:50 -0500

    improve logs

--- services/CypressService.js
+++ services/CypressService.js
@@ -2,6 +2,7 @@ var UtilsService = require('./UtilsService');
 var CypressService = {};
 
 CypressService.execute = testFile => {
+    console.log(`CypressService execute start: testFile = ${testFile}`);
     return UtilsService.executeCommand(`cypress run --spec ${testFile}`);
 };
 
